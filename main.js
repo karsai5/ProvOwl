48fb3fd929e8a1bb682caf5e6b431f2fb02ee13e
Linus Karsai <karsai5@gmail.com>
2015-10-06 13:14:27 +1100

    Cleaned up example css/javascript code.

--- main.js
+++ main.js
@@ -10,7 +10,7 @@ g.setDefaultEdgeLabel(function() { return {}; });
 // Add nodes to the graph. The first argument is the node id. The second is
 // metadata about the node. In this case we're going to add labels to each of
 // our nodes.
-g.setNode("kspacey",    { class: "kevin", label: "Kevin Spacey",  width: 144, height: 100 });
+g.setNode("kspacey",    { label: "Kevin Spacey",  width: 144, height: 100 });
 g.setNode("swilliams",  { label: "Saul Williams", width: 160, height: 100 });
 g.setNode("bpitt",      { label: "Brad Pitt",     width: 108, height: 100 });
 g.setNode("hford",      { label: "Harrison Ford", width: 168, height: 100 });
