340e67a3857bc2f6e96c72cb746354d654523e92
Linus Karsai <karsai5@gmail.com>
2015-10-23 15:24:42 +1100

    Added linting to vim. Made changes to make linter happy. Also tweaked test to compare manual graph against saved graph.

--- main.js
+++ main.js
@@ -1,89 +1,94 @@
-var PVisualiser = function(inner) {
-  console.log('Provenance visualiser initialised.');
-  this.inner = d3.select(inner);
-  this.g = new dagre.graphlib.Graph();
-  this.g.setGraph({});
-  this.g.setDefaultEdgeLabel(function() { return {}; });
-  this.g.graph().marginx = 20;
-  this.g.graph().marginy = 20;
+(function() {
+  /*jshint unused:false*/
+  "use strict";
 
-  this.createEntity = function(name, label) {
-    if (typeof name !== 'string' || typeof label !== 'string') {
-      throw new Error("Can't create entity: Unexpected Variables");
-    }
-    this.g.setNode(name, {shape: 'ellipse', class: 'entity', label: label});
-    return true;
-  };
+  var PVisualiser = function(inner) {
+    console.log('Provenance visualiser initialised.');
+    this.inner = d3.select(inner);
+    this.g = new dagre.graphlib.Graph();
+    this.g.setGraph({});
+    this.g.setDefaultEdgeLabel(function() { return {}; });
+    this.g.graph().marginx = 20;
+    this.g.graph().marginy = 20;
 
-  this.createActivity = function(name, label) {
-    if (typeof name !== 'string' || typeof label !== 'string') {
-      throw new Error("Can't create activity: Unexpected Variables");
-    }
-    this.g.setNode(name, {class: 'activity', label: label});
-    return true;
-  };
-
-  this.generated = function (name1, name2) {
-    if (typeof name1 !== 'string' || typeof name2 !== 'string') {
-      throw new Error("Can't create generation: Unexpected Variables");
-    }
-
-    // Check nodes exist
-    name1bool = false;
-    name2bool = false;
-    this.g.nodes().some(function(entry) {
-      if (entry === name1) {
-        name1bool = true;
-      } else if (entry === name2) {
-        name2bool = true;
+    this.createEntity = function(name, label) {
+      if (typeof name !== 'string' || typeof label !== 'string') {
+        throw new Error("Can't create entity: Unexpected Variables");
       }
-      if( name1bool === true && name2bool === true) {
-        return true;
+      var result = this.g.setNode(name, {shape: 'ellipse', class: 'entity', label: label});
+      return true;
+    };
+
+    this.createActivity = function(name, label) {
+      if (typeof name !== 'string' || typeof label !== 'string') {
+        throw new Error("Can't create activity: Unexpected Variables");
       }
-    });
+      this.g.setNode(name, {class: 'activity', label: label});
+      return true;
+    };
 
-    if (name1bool === false || name2bool === false) {
-      throw new Error("Can't create generation: Nonexistent node refereced");
-    }
-
-    this.g.setEdge(name1, name2, {label: 'gen'});
-    return true;
-  };
-
-  this.used = function (name1, name2) {
-    if (typeof name1 !== 'string' || typeof name2 !== 'string') {
-      throw new Error("Can't create use edge: Unexpected Variables");
-    }
-
-    // Check nodes exist
-    name1bool = false;
-    name2bool = false;
-    this.g.nodes().some(function(entry) {
-      if (entry === name1) {
-        name1bool = true;
-      } else if (entry === name2) {
-        name2bool = true;
+    this.generated = function (name1, name2) {
+      if (typeof name1 !== 'string' || typeof name2 !== 'string') {
+        throw new Error("Can't create generation: Unexpected Variables");
       }
-      if( name1bool === true && name2bool === true) {
-        return true;
+
+      // Check nodes exist
+      var name1bool = false;
+      var name2bool = false;
+      this.g.nodes().some(function(entry) {
+        if (entry === name1) {
+          name1bool = true;
+        } else if (entry === name2) {
+          name2bool = true;
+        }
+        if( name1bool === true && name2bool === true) {
+          return true;
+        }
+      });
+
+      if (name1bool === false || name2bool === false) {
+        throw new Error("Can't create generation: Nonexistent node refereced");
       }
-    });
 
-    if (name1bool === false || name2bool === false) {
-      throw new Error("Can't create use edge: Nonexistent node refereced");
-    }
+      this.g.setEdge(name1, name2, {label: 'gen'});
+      return true;
+    };
 
-    this.g.setEdge(name1, name2, {label: 'use'});
-    return true;
+    this.used = function (name1, name2) {
+      if (typeof name1 !== 'string' || typeof name2 !== 'string') {
+        throw new Error("Can't create use edge: Unexpected Variables");
+      }
+
+      // Check nodes exist
+      var name1bool = false;
+      var name2bool = false;
+      this.g.nodes().some(function(entry) {
+        if (entry === name1) {
+          name1bool = true;
+        } else if (entry === name2) {
+          name2bool = true;
+        }
+        if( name1bool === true && name2bool === true) {
+          return true;
+        }
+      });
+
+      if (name1bool === false || name2bool === false) {
+        throw new Error("Can't create use edge: Nonexistent node refereced");
+      }
+
+      this.g.setEdge(name1, name2, {label: 'use'});
+      return true;
+    };
+
+    this.render = function() {
+      var render = dagreD3.render();
+      this.inner.call(render, this.g);
+    };
+
+    this.getGraph = function () {
+      return this.g;
+    };
+
   };
-
-  this.render = function() {
-    var render = dagreD3.render();
-    this.inner.call(render, this.g);
-  };
-
-  this.getGraph = function () {
-    return this.g;
-  };
-
-};
+}());
