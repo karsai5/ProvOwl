951c201964ea979e1e50805d50f2823b98b1cdde
Linus Karsai <karsai5@gmail.com>
2015-12-02 11:12:39 +1100

    Allow custom styling of nodes via cytoscape.css

--- js/PVisualiser.js
+++ js/PVisualiser.js
@@ -20,6 +20,7 @@
       if (typeof name !== 'string' || typeof label !== 'string') {
         throw new Error("Can't create activity: Unexpected Variables");
       }
+      this.nodes.push({ data: { id: name, name: label, weight: 65, faveColor: '#6FB1FC', faveShape: 'triangle' } });
       return true;
     };
 
@@ -55,57 +56,26 @@
       if (typeof inner !== 'string') {
         throw new Error("Can't render graph: Unexpected Variables");
       }
-      $(inner).cytoscape({
-        layout: {
-          name: 'cose',
-          padding: 10
-        },
-        style: cytoscape.stylesheet()
-          .selector('node')
-          .css({
-            'shape': 'data(faveShape)',
-            'width': 'mapData(weight, 40, 80, 20, 60)',
-            'content': 'data(name)',
-            'text-valign': 'center',
-            'text-outline-width': 2,
-            'text-outline-color': 'data(faveColor)',
-            'color': '#fff'
-          })
-        .selector(':selected')
-          .css({
-            'border-width': 3,
-            'border-color': '#333'
-          })
-        .selector('edge')
-          .css({
-            'opacity': 0.666,
-            'width': 'mapData(strength, 70, 100, 2, 6)',
-            'target-arrow-shape': 'triangle',
-            'source-arrow-shape': 'circle',
-            'line-color': 'data(faveColor)',
-            'source-arrow-color': 'data(faveColor)',
-            'target-arrow-color': 'data(faveColor)'
-          })
-        .selector('edge.questionable')
-          .css({
-            'line-style': 'dotted',
-            'target-arrow-shape': 'diamond'
-          })
-        .selector('.faded')
-          .css({
-            'opacity': 0.25,
-            'text-opacity': 0
-          }),
-        elements: {
-          nodes: this.nodes,
-          edges: this.edges
-        },
+      this.inner = inner;
+      var that = this;
+      $.get('/css/cytoscape.css', function (data) {
+        $(that.inner).cytoscape({
+          layout: {
+            name: 'cose',
+            padding: 10
+          },
+          style: data,
+          elements: {
+            nodes: that.nodes,
+            edges: that.edges
+          },
 
-        ready: function(){
-          window.cy = this;
+          ready: function(){
+            window.cy = that;
 
-          // giddy up
-        }
+            // giddy up
+          }
+        });
       });
     };
 
