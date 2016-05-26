032c18aeb7563bdc2061c49258ff315022fefc91
Linus Karsai <karsai5@gmail.com>
2016-05-26 15:54:06 +1000

    Revert "Fixed downloading TSV in wrong format. Had to return to random download names."
    This reverts commit de7c10253bfe5c9c56c223baf85d5477f16eaeca.

--- src/js/pvis/PVisualiser.js
+++ src/js/pvis/PVisualiser.js
@@ -124,8 +124,7 @@ PVisualiser.prototype.unGroupNode = function(id, params) {
 	if (params.saveHistory === undefined) {
 		params.saveHistory = true;
 	}
-	if (params !== undefined && params.manual !== undefined && params.manual ===
-		true) {
+	if (params !== undefined && params.manual !== undefined && params.manual === true) {
 		pvis.clicks.add(new Click({
 			desc: "Ungrouped Node",
 			elementId: id
@@ -392,8 +391,7 @@ PVisualiser.prototype.groupSelectedNodes = function(params) {
 		return value.id();
 	});
 
-	if (params !== undefined && params.manual !== undefined && params.manual ===
-		true) {
+	if (params !== undefined && params.manual !== undefined && params.manual === true) {
 		pvis.clicks.add(new Click({
 			desc: "Grouped Nodes",
 			event: event,
